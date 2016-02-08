b88e6073f4e5d5fd4f2516bf4a736766dea55039
Jean Lucas <jeanleonino@gmail.com>
2016-02-08 01:31:26 -0300

    Added two modal styles

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,9 +2,17 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   isShowingModal: false,
+  isShowingBasic: false,
+  isShowingNotTranslucent: false,
   actions: {
     toggleModal: function() {
       this.toggleProperty('isShowingModal');
+    },
+    toggleBasic: function() {
+      this.toggleProperty('isShowingBasic');
+    },
+    toggleNotTranslucent: function() {
+      this.toggleProperty('isShowingNotTranslucent');
     }
   }
 });
